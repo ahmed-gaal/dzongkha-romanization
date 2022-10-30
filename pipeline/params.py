97d736e0073b9bdb91ac69ab19f4fcc6f9f4cbc0
Ahmed Ga'al <ahmedito14@gmail.com>
2022-10-30 20:00:22 +0300

    refactor: update project config

--- pipeline/params.py
+++ pipeline/params.py
@@ -7,6 +7,7 @@ class Params:
     original = assets_path / 'orignal' / 'data.csv'
     data = assets_path / 'dzongkha_data'
     features = assets_path / 'dzongkha_features'
+    probs = assets_path / 'frequencies'
     tokenizer = assets_path / 'tokenizers'
     models = assets_path / 'dzongkha_models'
     metrics = assets_path / 'metrics'
