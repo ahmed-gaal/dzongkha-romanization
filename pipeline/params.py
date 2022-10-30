f0a6d84a617725aadb544f02f5464d87eb12f8e6
Ahmed Ga'al <ahmedito14@gmail.com>
2022-10-30 15:08:05 +0300

    feat: update pipeline config

--- pipeline/params.py
+++ pipeline/params.py
@@ -7,5 +7,7 @@ class Params:
     original = assets_path / 'orignal' / 'data.csv'
     data = assets_path / 'dzongkha_data'
     features = assets_path / 'dzongkha_features'
+    tokenizer = assets_path / 'tokenizers'
     models = assets_path / 'dzongkha_models'
     metrics = assets_path / 'metrics'
+    pred = assets_path / 'predictions'
